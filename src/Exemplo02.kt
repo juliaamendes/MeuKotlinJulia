5ca13928c960fe3c033fea2abe562208402d9727
Luis <luismiguelxaviersousa1@gmail.com>
2025-08-13 10:07:01 -0300

    Initial commit

---
+++ src/Exemplo02.kt
@@ -0,0 +1,10 @@
+fun main(){
+    val nota = 6
+    val mensagem = when(nota){
+        in 7..10 -> "Aprovado"
+        in 5..6-> "Recuperação"
+        else -> "Reprovado"
+
+    }
+    println(mensagem)
+}
---
+++ src/Main.kt
@@ -0,0 +1,40 @@
+fun main(){
+    println("hello kotlin")
+
+    /*VARIAVEIS*/
+
+    val nome = "Julia"
+    var idade = 18
+
+
+    /*Tipos de dados*/
+    /*
+    INT = valores inteiros
+    Double= numeros decimais
+    Strings = texto
+    Boolean = true ou false
+     */
+
+
+    println("meu nome é " + nome + " tenho " + idade + " anos, esse é o meu primeiro kotlin")
+    /*Estrutura de controle de dados*/
+    val nota = 8
+    if (nota >= 7) {
+        println("Aprovado")
+    }
+    else {
+        println("Reprovado")
+
+    }
+    val dia= 4
+    when(dia){
+        1 -> println("Domingo")
+        2 -> println("Segunda")
+        3 -> println("Terça")
+        4 -> println("Quarta")
+        5 -> println("Quinta")
+        6 -> println("Sexta")
+        7 -> println("Sabado")
+        else -> println("Nenhuma das Opç")
+    }
+}
