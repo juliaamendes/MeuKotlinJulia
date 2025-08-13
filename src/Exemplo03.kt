a04e5d2f0c930c37ef3976dce40b05839d2fefcc
Luis <luismiguelxaviersousa1@gmail.com>
2025-08-13 10:12:54 -0300

    exemplo03

---
+++ src/Exemplo03.kt
@@ -0,0 +1,9 @@
+fun main(){
+    val pedido = "X-bacon"
+    when(pedido){
+        "X-bacon"-> println("Preço: 18,00")
+        "X-tudo"-> println("Preço: 26,00")
+        "X-salada"-> println("Preço: 17,50")
+        else -> println("item não encontrado no cardápio")
+    }
+}
